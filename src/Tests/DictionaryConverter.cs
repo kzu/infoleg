40e6de7446e28fd214fcf3037d8e3e39096ffc7d
Daniel Cazzulino <daniel@cazzulino.com>
2025-01-24 22:06:59 -0300

    Test yaml conversion and front-matter in markdown file

---
+++ src/Tests/DictionaryConverter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Clarius.OpenLaw;
+
+public class DictionaryConverter : JsonConverter<Dictionary<string, object?>>
+{
+    public override Dictionary<string, object?> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType != JsonTokenType.StartObject)
+        {
+            throw new JsonException();
+        }
+
+        var dictionary = new Dictionary<string, object?>();
+
+        while (reader.Read())
+        {
+            if (reader.TokenType == JsonTokenType.EndObject)
+            {
+                return dictionary;
+            }
+
+            if (reader.TokenType != JsonTokenType.PropertyName)
+            {
+                throw new JsonException();
+            }
+
+            string propertyName = reader.GetString() ?? throw new JsonException();
+
+            reader.Read();
+
+            dictionary[propertyName] = ReadValue(ref reader, options);
+        }
+
+        return dictionary;
+    }
+
+    object? ReadValue(ref Utf8JsonReader reader, JsonSerializerOptions options)
+    {
+        switch (reader.TokenType)
+        {
+            case JsonTokenType.StartObject:
+                return Read(ref reader, typeof(Dictionary<string, object?>), options);
+            case JsonTokenType.StartArray:
+                var list = new List<object?>();
+                while (reader.Read())
+                {
+                    if (reader.TokenType == JsonTokenType.EndArray)
+                    {
+                        return list;
+                    }
+
+                    list.Add(ReadValue(ref reader, options));
+                }
+                throw new JsonException();
+            case JsonTokenType.String:
+                return reader.GetString();
+            case JsonTokenType.Number:
+                if (reader.TryGetInt32(out int intValue))
+                {
+                    return intValue;
+                }
+                return reader.GetDouble();
+            case JsonTokenType.True:
+                return true;
+            case JsonTokenType.False:
+                return false;
+            case JsonTokenType.Null:
+                return null;
+            default:
+                throw new JsonException();
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, Dictionary<string, object?> value, JsonSerializerOptions options) 
+        => throw new NotImplementedException();
+}
---
+++ src/Tests/Misc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using Xunit;
+using YamlDotNet.Serialization;
+
+namespace Clarius.OpenLaw;
+
+public class Misc
+{
+    [Theory]
+    [InlineData(@"SaijSamples\123456789-0abc-defg-g81-87000tcanyel.json")]
+    public void ConvertJsonToYaml(string jsonFile)
+    {
+        var json = File.ReadAllText(jsonFile);
+
+        var options = new JsonSerializerOptions
+        {
+            Converters = { new DictionaryConverter() },
+        };
+
+        var dictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(json, options);
+
+        // Serialize the dictionary to YAML
+        var serializer = new SerializerBuilder().Build();
+        var yaml = serializer.Serialize(dictionary);
+
+        // Save the YAML to a file
+        var yamlFile = Path.ChangeExtension(jsonFile, ".yaml");
+        File.WriteAllText($@"..\..\..\{yamlFile}", yaml);
+    }
+}
